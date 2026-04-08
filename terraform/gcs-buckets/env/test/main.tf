886d2aeb9b46d8f800f3ba3fc6adce091fa4b938
Unil <unil4869@gmail.com>
2026-04-08 11:08:04 +0530

    gh

--- terraform/gcs-buckets/env/test/main.tf
+++ terraform/gcs-buckets/env/test/main.tf
@@ -1,17 +1,5 @@
-# Backend Configuration for Test Environment.
-# The backend configuration is provided via GitHub Actions during terraform init.
+# Backend is configured via terraform init in CI
 
-#terraform {
- # backend "gcs" {
-    # Configuration provided via init command:
-    # terraform init \
-    #   -backend-config="bucket=${GCS_BU_TASK_TEST_STATE_BUCKET}" \
-    #   -backend-config="prefix=${GCS_BU_TASK_TEST_STATE_PREFIX}" \
-  #}
-#}
-
-# ✅ Decode JSON from GitHub secret
 locals {
   config = jsondecode(var.gcs_bu_task_test_json)
-
 }
