0278fa950a52b44a94cdb5895ea46d0828e64c40
Unil <unil4869@gmail.com>
2026-04-08 12:53:21 +0530

    ds

--- terraform/gcs-buckets/env/test/outputs.tf
+++ terraform/gcs-buckets/env/test/outputs.tf
@@ -1,29 +1,33 @@
 # Output Values for Test Environment
-
 output "test_bucket_names" {
   description = "List of all created bucket names in test environment"
-  value       = module.gcs_buckets_test.bucket_names
+  value       = keys(google_storage_bucket.buckets)
 }
 
 output "test_bucket_urls" {
   description = "Map of bucket names to their GS URLs in test environment"
-  value       = module.gcs_buckets_test.bucket_urls
+  value = {
+    for b in google_storage_bucket.buckets :
+    b.name => b.url
+  }
 }
 
 output "test_bucket_details" {
   description = "Detailed information about all created buckets in test environment"
-  value       = module.gcs_buckets_test.bucket_details
+  value       = google_storage_bucket.buckets
   sensitive   = true
 }
 
 output "test_iam_assignments" {
   description = "Summary of IAM role assignments in test environment"
-  value       = module.gcs_buckets_test.iam_assignments
+  value       = "Not implemented"
   sensitive   = true
 }
 
 output "test_environment_summary" {
   description = "High-level summary of the test deployment"
-  value       = module.gcs_buckets_test.environment_summary
-  sensitive   = true
+  value = {
+    total_buckets = length(google_storage_bucket.buckets)
+  }
+  sensitive = true
 }
